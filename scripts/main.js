75384b0b30ded688fab926694b0a61dd366737ad
Oskar Rough <oskarrough@gmail.com>
2013-06-27 14:25:39 +0200

    Make everything nicer

--- scripts/main.js
+++ scripts/main.js
@@ -1,48 +1,9 @@
 $(function() {
 
-	/* Nav highlighting */
-	$('.Section')
-	.waypoint(function(direction) {
-		var $links = $('a[href="#' + this.id + '"]');
-		$links.toggleClass('is-active', direction === 'down');
-
-	}, {
-		//context: '.l-Primary',
-		offset: '100%'
-	})
-	.waypoint(function(direction) {
-		var $links = $('a[href="#' + this.id + '"]');
-		$links.toggleClass('is-active', direction === 'up');
-	}, {
-		//context: '.l-Primary',
-		offset: function() {
-			return -$(this).height();
-		}
-	});
-  	// make sure waypoints are correct
-  	$.waypoints('refresh');
-
-
-	/* Generate a menu from the content on the page */
-	var $nav = $('.js-genenav');
-	$('.Section').each(function(element){
-		myID = $(this).attr('id');
-
-		if (myID) {
-			$nav.append('<li><a href="#'+myID+'"> '+myID+' </a></li>');
-		}
-	});
-
-
-	/* Smooth scrolling instead of jumping */
-	$nav.find('a').on('click', jump);
-	function jump(event) {
-		var $active = $(event.currentTarget);
-		var offset = $( $active.attr('href') ).offset().top;
-		$('html, body').animate({
-			scrollTop: offset
-		}, 250, function() {
-		});
-	}
+	console.log("We're running…");
 
 });
+
+window.onload = function() {
+  new StickyTitles( $('.Title') );
+};
