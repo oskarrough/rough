0a2b0779bd7fd185ce26cea73a8b4e82cbe6bceb
Oskar Rough <oskarrough@gmail.com>
2013-08-14 14:26:57 +0200

    Improve and document the use of masonry.js

--- app/scripts/masonry.js
+++ app/scripts/masonry.js
@@ -2,16 +2,27 @@
 // Dynamic grid with Masonry
 //
 
-$(function () {
-	var $container = $('.js-masonry');
-	if ($container.length > 0) {
-		$container.prepend('<div class="grid-sizer"></div><div class="gutter-sizer"></div>');
-		// $container.imagesLoaded( function() {
-		// 	$container.masonry({
-		// 		columnWidth: '.grid-sizer',
-		// 		gutter: '.gutter-sizer',
-		// 		itemSelector: '.Teaser'
-		// 	});
-		// });
-	}
-});
+// choose the container for the grid
+var container = document.getElementById('Masonry');
+
+// @todo: rewrite this, dont wrap everything in a conditional
+// only run if the container exists
+if ($(container).length > 0) {
+
+	// insert elements masonry needs for better fluid measurements
+	$(container).prepend('<div class="grid-sizer"></div><div class="gutter-sizer"></div>');
+
+	//$(function () {
+
+		// enable masonry after images are loaded
+		imagesLoaded(container, function() {
+			var msnry = new Masonry( container, {
+				columnWidth: '.grid-sizer',
+				gutter: '.gutter-sizer',
+				itemSelector: '.Article'
+			});
+		});
+
+	//});
+
+}
