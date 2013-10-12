7c59cfdaa02c9ac2843e3a501d1f5263612571cc
Oskar Rough <oskarrough@gmail.com>
2013-10-12 14:41:55 +0200

    Clarify Masonry/Dynamicgrid

--- app/scripts/masonry.js
+++ app/scripts/masonry.js
@@ -1,28 +1,30 @@
-//
-// Dynamic grid with Masonry
-//
+/**
+ * Dynamic grid with Masonry
+ */
 
-// choose the container for the grid
-var container = document.getElementById('Masonry');
+var DynamicGrid = function() {};
 
-// @todo: rewrite this, dont wrap everything in a conditional
-// only run if the container exists
-if ($(container).length > 0) {
+DynamicGrid.prototype = {
 
-	// insert elements masonry needs for better fluid measurements
-	$(container).prepend('<div class="grid-sizer"></div><div class="gutter-sizer"></div>');
+	$container: $('#Masonry'),
 
-	//$(function () {
+	init: function() {
+		this.$container.prepend('<div class="Masonry-gridSizer"></div><div class="Masonry-gutterSizer"></div>');
 
 		// enable masonry after images are loaded
-		imagesLoaded(container, function() {
-			var msnry = new Masonry( container, {
-				columnWidth: '.grid-sizer',
-				gutter: '.gutter-sizer',
-				itemSelector: '.Article'
-			});
-		});
+		this.$container.imagesLoaded(this.runMasonry());
+	},
 
-	//});
+	runMasonry: function() {
+		this.$container.masonry({
+			columnWidth: '.Masonry-gridSizer',
+			gutter: '.Masonry-gutterSizer',
+			itemSelector: '.Masonry-item'
+		}).addClass('is-active');
+	}
+};
 
-}
+var dynamicGrid = new DynamicGrid();
+
+// DO IT
+dynamicGrid.init();
