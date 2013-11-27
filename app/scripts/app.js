3b97034180194897961507e7fd44e97bb56f1a48
Oskar Rough <oskarrough@gmail.com>
2013-11-27 13:59:45 +0100

    Clean up

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,14 +1,12 @@
 /*global Masonry,Nav*/
 
+
 /**
  * Call everything from this file
  */
-
-
 $(function(){
 
 	var masonry = new Masonry();
 	var nav = new Nav();
-	//$('.Nav--togglable').roughNavToggle();
 
 });
