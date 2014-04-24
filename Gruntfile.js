c1a67f9350076c3dc94f1236222b2d2b048e448d
Oskar Rough <oskarrough@gmail.com>
2014-04-24 17:52:12 +0200

    Remove unused stuff from gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -31,7 +31,6 @@ module.exports = function (grunt) {
 			gruntfile: {
 				files: ['Gruntfile.js']
 			},
-
 			// compass: {
 			// 	files: ['<%= config.app %>/styles/{,*/}*.{scss,sass}'],
 			// 	tasks: ['compass:server', 'autoprefixer']
@@ -283,32 +282,6 @@ module.exports = function (grunt) {
 				}]
 			}
 		},
-
-		// By default, your `index.html`'s <!-- Usemin block --> will take care of
-		// minification. These next options are pre-configured if you do not wish
-		// to use the Usemin blocks.
-		// cssmin: {
-		//     dist: {
-		//         files: {
-		//             '<%%= yeoman.dist %>/styles/main.css': [
-		//                 '.tmp/styles/{,*/}*.css',
-		//                 '<%%= yeoman.app %>/styles/{,*/}*.css'
-		//             ]
-		//         }
-		//     }
-		// },
-		// uglify: {
-		//     dist: {
-		//         files: {
-		//             '<%%= yeoman.dist %>/scripts/scripts.js': [
-		//                 '<%%= yeoman.dist %>/scripts/scripts.js'
-		//             ]
-		//         }
-		//     }
-		// },
-		// concat: {
-		//     dist: {}
-		// },
 
 		// Copies remaining files to places other tasks can use
 		copy: {
