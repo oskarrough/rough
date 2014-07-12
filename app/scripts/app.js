f32231d72e9d9d97d4ec5c764b79f9665427d236
Oskar Rough <oskarrough@gmail.com>
2014-07-12 14:30:55 +0200

    Refactor masonry code - renamed to rough-masonry (to distinguish from standard masonry) - cleaned up everything, more readable compact code - added some readme - added pointers for fixed vs fluid width - added a method to fade in items one after another

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,10 +2,8 @@
 $(function(){
 
 	// console.log('Running app.js');
-
 	// var masonry = new Masonry();
 	// var nav = new Nav();
 	// var video = new Video();
 	var tab = new Tab();
-
 });
---
+++ app/scripts/rough-masonry.js
@@ -0,0 +1,78 @@
+/*jshint unused:false */
+
+/**
+ * Dynamic grid with Masonry
+ */
+var Masonry = function(el, options) {
+
+	// Defaults:
+	this.defaults = {
+		container: '#Masonry'
+	};
+
+	// Extending options:
+	this.opts = $.extend({}, this.defaults, options);
+
+	// If an element was passed, use it, otherwise use a default el
+	if (el) {
+		this.$el = $(el);
+	} else {
+		this.$el = $(this.defaults.container);
+	}
+
+	this.init();
+};
+
+Masonry.prototype = {
+	isFirstRun: true,
+
+	init: function() {
+
+		// Only run if the container exists
+		if (this.$el.length < 1 ) {
+			console.log('Didn\'t find a Masonry container');
+			return false;
+		}
+
+		// Only needed if your items have percentage/fluid widths
+		if (this.isFirstRun) {
+			this.$el.prepend('<div class="Masonry-gridSizer"></div><div class="Masonry-gutterSizer"></div>');
+			this.isFirstRun = false;
+		}
+
+		// Run after images are loaded
+		this.$el.imagesLoaded( $.proxy(this.runMasonry, this) );
+
+		// Just run
+		// this.runMasonry();
+	},
+
+	runMasonry: function() {
+		this.$el.masonry({
+			itemSelector: '.Masonry-item',
+			gutter: '.Masonry-gutterSizer', // replace with a number if you have fixed widths
+			columnWidth: '.Masonry-gridSizer' // replace with a number if you have fixed widths
+		}).addClass('is-active');
+
+		// layout again
+		// this.$el.masonry();
+
+		// layout again after all images are loaded
+		// this.$el.imagesLoaded( function(){
+		// 	this.$el.masonry();
+		// });
+	},
+
+	/**
+	 * Fade in items one after another as the images load
+	 * remember to add the corresponding css
+	 */
+	fadeInItems: function() {
+		var $items = this.$el.find('.Masonry-item');
+		$items.each(function(index, el){
+			$(this).imagesLoaded( function() {
+				$(el).addClass('is-loaded');
+			});
+		});
+	}
+};
