5a20e8e18ce72b0d2082367a0a74d80f26f523b7
Lisa P <mail@lislis.de>
2014-07-14 13:45:41 +0200

    prepare tab styles

--- app/scripts/tab.js
+++ app/scripts/tab.js
@@ -16,7 +16,34 @@ var Tab = function() {
 Tab.prototype = {
 
 	init: function() {
-		
+		this.activateFirst();
+		this.actions();
+	},
+	/*
+	* if no element is active on init, activate the first one
+	*/
+	activateFirst: function() {
+		if($('.Tabs-nav .Tabs-is-active').length === 0) {
+			$('.Tabs-nav li').eq(0).addClass('Tabs-is-active');
+			$('.Tabs-item').eq(0).addClass('Tabs-is-active');
+		}
+	},
+	/*
+	* toggle the tab items
+	*/
+	toggleActive: function(e) {
+		var $target = $(e.target);
+		var pane = $target.attr('href');
+
+		$('.Tabs-is-active').removeClass('Tabs-is-active');
+		$target.parent('li').addClass('Tabs-is-active');
+		$(pane).addClass('Tabs-is-active');
+	},
+	/*
+	* bind actions
+	*/
+	actions: function() {
+		$('.Tabs-nav a').on('click', $.proxy(this.toggleActive, this));
 	}
 
 };
