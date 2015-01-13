78700cb9ce844bed0e95435a1e4020e86da142cd
Oskar Rough <oskarrough@gmail.com>
2015-01-13 16:24:25 +0100

    Update build system to newest generator-gulp-webapp standards

--- gulpfile.js
+++ gulpfile.js
@@ -25,22 +25,23 @@ gulp.task('jshint', function () {
 		.pipe($.jshint.reporter('fail'));
 });
 
-// Compile jade into HMTL
-gulp.task('jade', function () {
-	return gulp.src(['app/*.jade'])
+// Compile .jade into .html in the .tmp dir
+gulp.task('views', function () {
+	return gulp.src('app/*.jade')
 		.pipe($.jade({pretty: true}))
 		.pipe(gulp.dest('.tmp'));
 });
 
-gulp.task('html', ['jade', 'styles'], function () {
+gulp.task('html', ['views', 'styles'], function () {
 	var assets = $.useref.assets({searchPath: '{.tmp,app}'});
 
-	return gulp.src('.tmp/*.html')
+	return gulp.src(['app/*.html', '.tmp/*.html'])
 		.pipe(assets)
 		.pipe($.if('*.js', $.uglify()))
 		.pipe($.if('*.css', $.csso()))
 		.pipe(assets.restore())
 		.pipe($.useref())
+		// .pipe($.if('*.html', $.minifyHtml({conditionals: true, loose: true})))
 		.pipe(gulp.dest('dist'));
 });
 
@@ -64,7 +65,8 @@ gulp.task('extras', function () {
 	return gulp.src([
 		'app/*.*',
 		'!app/*.html',
-		'!app/**/*.jade'
+		'!app/*.jade',
+		'node_modules/apache-server-configs/dist/.htaccess'
 	], {
 		dot: true
 	}).pipe(gulp.dest('dist'));
@@ -78,13 +80,13 @@ gulp.task('icons', function () {
 
 gulp.task('clean', require('del').bind(null, ['.tmp', 'dist']));
 
-gulp.task('connect', ['styles'], function () {
+gulp.task('connect', ['views', 'styles'], function () {
 	var serveStatic = require('serve-static');
 	var serveIndex = require('serve-index');
 	var app = require('connect')()
 		.use(require('connect-livereload')({port: 35729}))
-		.use(serveStatic('app'))
 		.use(serveStatic('.tmp'))
+		.use(serveStatic('app'))
 		// paths to bower_components should be relative to the current file
 		// e.g. in app/index.html you should use ../bower_components
 		.use('/bower_components', serveStatic('bower_components'))
@@ -93,33 +95,46 @@ gulp.task('connect', ['styles'], function () {
 	require('http').createServer(app)
 		.listen(9000)
 		.on('listening', function () {
-			console.log('Started connect web server on http://0.0.0.0:9000');
+			console.log('Started connect web server on http://localhost:9000');
 		});
 });
 
-gulp.task('serve', ['html', 'connect', 'watch'], function () {
-	require('opn')('http://0.0.0.0:9000');
+gulp.task('serve', ['connect', 'watch'], function () {
+	require('opn')('http://localhost:9000');
+});
+
+// inject bower components
+gulp.task('wiredep', function () {
+	var wiredep = require('wiredep').stream;
+
+	gulp.src('app/styles/*.scss')
+		.pipe(wiredep())
+		.pipe(gulp.dest('app/styles'));
+
+	gulp.src('app/templates/layout.jade')
+		.pipe(wiredep())
+		.pipe(gulp.dest('app/templates'));
 });
 
 gulp.task('watch', ['connect'], function () {
 	$.livereload.listen();
 
-	// Notify livereload when these files change
+	// watch for changes
 	gulp.watch([
 		'app/*.html',
 		'.tmp/*.html',
-		'app/templates/*.jade',
 		'.tmp/styles/**/*.css',
 		'app/scripts/**/*.js',
 		'app/images/**/*'
 	]).on('change', $.livereload.changed);
 
 	// Run these tasks when these files change
-	gulp.watch('app/*.jade', ['jade']);
+	gulp.watch('app/**/*.jade', ['views']);
 	gulp.watch('app/styles/**/*.scss', ['styles']);
+	gulp.watch('bower.json', ['wiredep']);
 });
 
-gulp.task('build', ['jshint', 'html', 'images', 'extras', 'icons'], function () {
+gulp.task('build', ['jshint', 'html', 'images', 'fonts', 'extras', 'icons'], function () {
 	return gulp.src('dist/**/*').pipe($.size({title: 'build', gzip: true}));
 });
 
