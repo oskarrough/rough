c61ceee415a7d597f9888ae222d21b019eb87632
Oskar Rough <oskarrough@gmail.com>
2015-09-19 11:37:16 +0200

    Get rid of grunt by using grunticon-lib directly

--- gulp/icons.js
+++ gulp/icons.js
@@ -1,8 +1,23 @@
-const gulp = require('gulp');
-const shell = require('gulp-shell');
+// Run grunticon through gulp.
+// - https://github.com/filamentgroup/gulpicon
+// - https://gist.github.com/dcalhoun/e79ad10d518612d70721
 
-// Runs grunticon directly from grunt
-// @todo find a standalone grunticon tool
-gulp.task('icons', shell.task([
-	'grunt grunticon'
-]));
+const gulp = require('gulp');
+const Grunticon = require('grunticon-lib');
+const q = require('q');
+const path = require('path');
+const fs = require('fs');
+
+gulp.task('icons', () => {
+	const deferred = q.defer();
+	const iconDir = 'app/images/icons/';
+	const options = { enhanceSVG: true };
+	const files = fs.readdirSync(iconDir).map(fileName => path.join(iconDir, fileName));
+	const grunticon = new Grunticon(files, '.tmp/images/icons', options);
+
+	grunticon.process(() => {
+		deferred.resolve();
+	});
+
+	return deferred.promise;
+});
