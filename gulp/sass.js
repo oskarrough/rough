e6b0c049b2b2948b7e3470458fabc190254786ee
Oskar Rough <oskarrough@gmail.com>
2015-09-20 23:53:30 +0200

    Use autoprefixer through postcss

--- gulp/sass.js
+++ gulp/sass.js
@@ -1,22 +1,21 @@
-const gulp = require('gulp');
-const sass = require('gulp-sass');
-const plumber = require('gulp-plumber');
-const sourcemaps = require('gulp-sourcemaps');
-const autoprefixer = require('gulp-autoprefixer');
+const autoprefixer = require('autoprefixer');
 const browserSync = require('./serve');
+const gulp = require('gulp');
+const plumber = require('gulp-plumber');
+const postcss = require('gulp-postcss');
+const sass = require('gulp-sass');
+const sourcemaps = require('gulp-sourcemaps');
+
+// Compiles Sass with autoprefixer and sourcemaps
+const browsers = ['last 2 versions', 'android 4', 'ios 7', 'ie 10'];
 
 gulp.task('sass', () => {
 	return gulp.src('app/styles/*.scss')
 		.pipe(plumber())
 		.pipe(sourcemaps.init())
-		.pipe(sass.sync({
-			outputStyle: 'expanded',
-			precision: 10,
-			includePaths: ['.']
-		}).on('error', sass.logError))
-		.pipe(autoprefixer({
-			browsers: ['last 2 version', 'android 4', 'ios 7', 'ie 10']
-		}))
+		.pipe(sass.sync({ precision: 10, includePaths: ['.'] })
+			.on('error', sass.logError))
+		.pipe(postcss([autoprefixer({ browsers })]))
 		.pipe(sourcemaps.write())
 		.pipe(gulp.dest('.tmp/styles'))
 		.pipe(browserSync.stream());
