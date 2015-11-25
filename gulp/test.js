35542acdf22b083c75326ecf792a086597a440f0
Oskar Rough <oskarrough@gmail.com>
2015-11-25 17:46:44 +0100

    Replace gulp test with npm test

--- gulp/test.js
+++
@@ -1,8 +0,0 @@
-const gulp = require('gulp');
-const shell = require('gulp-shell');
-
-// Lints our scripts with XO as defined in `package.json`.
-gulp.task('test', callback => {
-	shell.task(['xo']);
-	callback();
-});
