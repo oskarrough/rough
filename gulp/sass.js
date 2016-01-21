fc68cbce20a5607a807d4f46bf2093b1486f86b9
Oskar Rough <oskarrough@gmail.com>
2016-01-21 15:49:43 +0100

    Add support for importing CSS files and automatically inlining any import statements

--- gulp/sass.js
+++ gulp/sass.js
@@ -2,20 +2,36 @@ const autoprefixer = require('autoprefixer');
 const browserSync = require('./serve');
 const gulp = require('gulp');
 const plumber = require('gulp-plumber');
-const postcss = require('gulp-postcss');
 const sass = require('gulp-sass');
 const sourcemaps = require('gulp-sourcemaps');
+const path = require('path');
+const postcss = require('gulp-postcss');
+const atImport = require('postcss-import');
 
 // Compiles Sass with autoprefixer and sourcemaps
 const browsers = ['ie >= 10', 'ie_mob >= 10', 'ff >= 30', 'chrome >= 34', 'safari >= 7', 'opera >= 23', 'ios >= 7', 'android >= 4.4', 'bb >= 10'];
+
+const sassOptions = {
+	// Fluid grids require more than 3 decimals…
+	precision: 10,
+	// Also look in npm and bower
+	includePaths: [
+		'.',
+		path.join(__dirname, '../node_modules'),
+		path.join(__dirname, '../bower_components')
+	]
+};
 
 gulp.task('sass', () => {
 	return gulp.src('app/styles/*.scss')
 		.pipe(plumber())
 		.pipe(sourcemaps.init())
-		.pipe(sass.sync({precision: 10, includePaths: ['.']})
+		.pipe(sass.sync(sassOptions)
 			.on('error', sass.logError))
-		.pipe(postcss([autoprefixer({browsers})]))
+		.pipe(postcss([
+			atImport(),
+			autoprefixer({browsers})
+		]))
 		.pipe(sourcemaps.write())
 		.pipe(gulp.dest('.tmp/styles'))
 		.pipe(browserSync.stream());
