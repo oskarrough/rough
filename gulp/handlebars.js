630ac7e7e4214b5683deb225c0e5e4ab49e41ad1
Oskar Rough <oskarrough@gmail.com>
2016-03-06 02:23:03 +0100

    Update packages

--- gulp/handlebars.js
+++ gulp/handlebars.js
@@ -3,19 +3,19 @@
 const gulp = require('gulp');
 const hb = require('gulp-hb');
 const layouts = require('handlebars-layouts');
-hb.handlebars.registerHelper(layouts(hb.handlebars));
 const notify = require('gulp-notify');
 const browserSync = require('./serve');
 
 gulp.task('handlebars', () => {
+	const hbStream = hb({bustCache: true})
+		.helpers(layouts)
+		.helpers('app/templates/helpers/**/*.js')
+		.partials('app/templates/*.hbs')
+		.data('app/templates/data/**/*.{js,json}');
+
 	return gulp.src('app/*.html')
-		.pipe(hb({
-			data: 'app/templates/data/**/*.{js,json}',
-			helpers: 'app/templates/helpers/*.js',
-			partials: 'app/templates/*.hbs',
-			bustCache: true
-		}))
-		.on('error', notify.onError(error => `Handlebars error: ${error}`))
+		.pipe(hbStream)
+			.on('error', notify.onError(error => `Handlebars error: ${error}`))
 		.pipe(gulp.dest('.tmp'))
 		.pipe(browserSync.stream());
 });
