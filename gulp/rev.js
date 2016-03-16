b4ac7f1a60f67eeecf88e38b9f7942e4c1aaf76b
Oskar Rough <oskarrough@gmail.com>
2016-03-16 15:00:19 +0100

    Clean up after rev task

--- gulp/rev.js
+++ gulp/rev.js
@@ -1,6 +1,5 @@
 const gulp = require('gulp');
 const RevAll = require('gulp-rev-all');
-// const revNapkin = require('gulp-rev-napkin');
 
 gulp.task('rev', () => {
 	const revAll = new RevAll({
@@ -11,9 +10,6 @@ gulp.task('rev', () => {
 	return gulp.src(['.tmp/**'])
 		.pipe(revAll.revision())
 		.pipe(gulp.dest('dist'))
-		// .pipe(revNapkin()) // Clean up original files.
 		.pipe(revAll.versionFile())
-		.pipe(gulp.dest('dist'))
-		.pipe(revAll.manifestFile())
 		.pipe(gulp.dest('dist'));
 });
