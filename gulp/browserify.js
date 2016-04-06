0e614c1860d1d579db1627a0cdd5b6d065d9c26d
Oskar Rough <oskar@rough.dk>
2016-04-06 22:26:36 +0200

    Add rollupify for potentially smaller js bundles

--- gulp/browserify.js
+++ gulp/browserify.js
@@ -2,6 +2,7 @@ const gulp = require('gulp');
 const source = require('vinyl-source-stream');
 const browserify = require('browserify');
 const babelify = require('babelify');
+const rollupify = require('rollupify');
 const browserSync = require('./serve');
 const browserifyShim = require('browserify-shim');
 const notify = require('gulp-notify');
@@ -9,6 +10,7 @@ const notify = require('gulp-notify');
 // Runs browserify with transforms on our scripts
 gulp.task('browserify', () => {
 	return browserify('app/scripts/index.js', {debug: true})
+		.transform(rollupify)
 		.transform(babelify)
 		.transform(browserifyShim)
 		.bundle()
